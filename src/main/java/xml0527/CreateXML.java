bc2ebb33c0a829230c317321cf0261b582aea51b
gyumin6349 <shjobo2@naver.com>
2025-05-27 12:33:50 +0900

    두번째 커밋

--- src/main/java/xml0527/CreateXML.java
+++ src/main/java/xml0527/CreateXML.java
@@ -1,10 +1,50 @@
 package xml0527;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
 public class CreateXML {
 
+	public void createXML() {
+		//1. XML 문서객체 생성
+		Document doc = new Document();
+		
+		//2. 최상위 부모노드 생성
+		Element rootNode = new Element("root");
+		
+		//3. 자식 노드생성
+		Element msgNode = new Element("msg");
+		//자식 노드에 값설정
+		msgNode.setText("안녕하세요?");
+		System.out.println(msgNode);
+		System.out.println(rootNode);
+		//자식 노드를 부모노드에 배치
+		rootNode.addContent(msgNode);
+		//모든 자식 노드를 가진 부모노드를 문서 객체에 배치
+		doc.addContent(rootNode);
+		
+		//출력객체 생성
+//		XMLOutputter xOut = new XMLOutputter(Format.getRawFormat());
+//		XMLOutputter xOut = new XMLOutputter(Format.getCompactFormat());
+		XMLOutputter xOut = new XMLOutputter(Format.getPrettyFormat());
+		try {
+			//콘솔출력
+			xOut.output(doc, System.out);
+			//파일로 출력
+			xOut.output(doc, new FileOutputStream("C:/dev/workspace/jsp_prj/src/main/webapp/xml0527/create.xml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new CreateXML().createXML();
 	}
 
 }
---
+++ src/main/webapp/xml0527/create_xml.jsp
@@ -0,0 +1,41 @@
+<%@page import="org.jdom2.output.Format"%>
+<%@page import="java.io.IOException"%>
+<%@page import="java.io.FileOutputStream"%>
+<%@page import="org.jdom2.output.XMLOutputter"%>
+<%@page import="org.jdom2.Element"%>
+<%@page import="org.jdom2.Document"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+   pageEncoding="UTF-8" trimDirectiveWhitespaces="true" info=""%>
+   
+<%
+	//1. XML 문서객체 생성
+	Document doc = new Document();
+	
+	//2. 최상위 부모노드 생성
+	Element rootNode = new Element("root");
+	
+	//3. 자식 노드생성
+	Element msgNode = new Element("msg");
+	//자식 노드에 값설정
+	msgNode.setText("안녕하세요?");
+	System.out.println(msgNode);
+	System.out.println(rootNode);
+	//자식 노드를 부모노드에 배치
+	rootNode.addContent(msgNode);
+	//모든 자식 노드를 가진 부모노드를 문서 객체에 배치
+	doc.addContent(rootNode);
+	
+	//출력객체 생성
+//	XMLOutputter xOut = new XMLOutputter(Format.getRawFormat());
+//	XMLOutputter xOut = new XMLOutputter(Format.getCompactFormat());
+	XMLOutputter xOut = new XMLOutputter(Format.getPrettyFormat());
+	try {
+		//콘솔출력
+		xOut.output(doc, out);
+		//파일로 출력
+		xOut.output(doc, new FileOutputStream("C:/dev/workspace/jsp_prj/src/main/webapp/xml0527/create.xml"));
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+%>
